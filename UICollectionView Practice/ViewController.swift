7c56a3a4def92a630ded3ab01f43f9499c699598
DongMing <hdmdhr@gmail.com>
2018-09-25 20:16:41 +0800

    Initial Commit

---
+++ UICollectionView Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionView Practice
+//
+//  Created by 胡洞明 on 2018/9/25.
+//  Copyright © 2018年 胡洞明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
